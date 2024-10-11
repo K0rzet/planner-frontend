23b11a038fc6ec0fbff884d7d57c42cc28ab8d25
K0rzet <ehot2004@gmail.com>
2024-10-11 13:07:16 +0300

    changed: urls for production

--- src/components/dashboard-layout/sidebar/Sidebar.tsx
+++ src/components/dashboard-layout/sidebar/Sidebar.tsx
@@ -39,16 +39,6 @@ export function Sidebar() {
 				</div>
 			</div>
 			<footer className='text-xs opacity-40 font-normal text-center p-layout'>
-				2024 &copy; With love from{' '}
-				<a
-					href='https://www.youtube.com/c/redgroup/?sub_confirmation=1'
-					target='_blank'
-					rel='noreferrer'
-					className='hover:text-primary text-brand-300 transition-colors'
-				>
-					RED Group
-				</a>
-				. <br /> All rights reserved.
 			</footer>
 		</aside>
 	)
